de76c56aaf4b3769d5b7d99e2d393ee0cf99a80c
Arunabho Majumder <arunabho95@gmail.com>
2018-09-30 20:39:51 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,11 +10,11 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
             // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            if (e.direction=="right") {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
             }
             else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
             }
         }
     }
